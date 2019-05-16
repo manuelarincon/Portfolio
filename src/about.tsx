c37349dd82783abb84674e1d2eaf6271d03bd974
Manuela Rincon <manuelarincon@Manuelas-MacBook-Pro.local>
2019-05-16 17:21:49 -0500

    more UI changes

--- src/about.tsx
+++ src/about.tsx
@@ -15,19 +15,19 @@ export class Me extends React.Component {
                     I got my degree in Design in 2017 from The University of Texas at Austin, Hook-em.
                     <br/> <br />
                     Currently I'm expanding on my programing capacities and will be improving this site on a consistent basis. <br />
-                    You can check out some of my coding projects and how I built this website on my 
-                    <a href ="https://github.com/manuelarincon/Portfolio" > GitHub.</a>
+                    You can check out some of my coding projects and how I built this website on my &#8204;
+                    <a href ="https://github.com/manuelarincon/Portfolio" >GitHub.</a>
                     <br /> <br />
                     When I’m not working, learning something new or taking a nap you can find me either reading at local coffee shop, the movies,
                     with family or planing a future trip. One of my goals is to see as much of the world as I can and would love any suggestions on 
                     cool and noteworthy places you have experienced. I’m a bit of a daredevil and have gone skydiving and paragliding so don’t 
-                    hesitate in sending me 
-                    <Link to='/MessageForm'> your recommendations! </Link>
-                    &nbsp;I can also propose some ideas, just let me know 
-                    <Link to='/MessageForm'> what kind of trips you like. </Link>
+                    hesitate in sending me &#8204;
+                    <Link to='/MessageForm'>your recommendations! </Link>
+                    &nbsp;I can also propose some ideas, just let me know &#8204; 
+                    <Link to='/MessageForm'>what kind of trips you like. </Link>
                     <br /> <br />
-                    Looking for inspiration? Here are a couple of my 
-                    <a href ="https://paper.dropbox.com/doc/Some-awesome-humans--AWnHdhSIJ6tfnG4ZVnAWXkKhAQ-qc7AKJv0obE7H5fYc90WQ" target="_new"> favorite people </a> 
+                    Looking for inspiration? Here are a couple of my &#8204;
+                    <a href ="https://paper.dropbox.com/doc/Some-awesome-humans--AWnHdhSIJ6tfnG4ZVnAWXkKhAQ-qc7AKJv0obE7H5fYc90WQ" target="_new">favorite people </a> 
                     I draw creativity and motivation from.
                     <br /> <br />
                     Interested in working together? 
